3b4ed28c86188c5cd009d5820f3fdb1d4eb3130d
Ryan Mosely <ryan.charles.mosely@gmail.com>
2020-01-09 18:26:24 -0800

    edited html

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-/*
+
 var connection = mysql.createConnection({
   host: "mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
   password: "rcjs7cgdu6inspy0",
   database: "gfhiwsgbw09lks9v"
 });
-*/
+
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);   
 } else {
@@ -21,6 +21,8 @@ if (process.env.JAWSDB_URL) {
     })
 }
 
+//COMMENT THIS OUT WHEN CREAT POOL
+/////////////////////////////////////////////
 // Make connection.
 connection.connect(function(err) {
   if (err) {
@@ -29,6 +31,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+/////////////////////////////////////////////
 
 // Export connection for our ORM to use.
 module.exports = connection;
